a8492e51c2e4a3603f00d0506932afcc09158e95
Leonid Golikhin <korrnals@gmail.com>
2026-02-09 17:15:16 +0300

    feat(labels): add structured labels CLI commands
    Add new cmd/labels/ package with proper command hierarchy:
    - gotr labels update [test|tests]
    - Support for --labels, --run-id, --test-ids flags
    - 12 tests for parsing and validation
    
    Update cmd/update.go to include 'labels' as deprecated alias:
    - gotr update labels <test_id> --labels=a,b
    - Shows deprecation warning pointing to new command
    
    Step 2 of Stage 5: CLI Coverage

--- cmd/commands.go
+++ cmd/commands.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/Korrnals/gotr/cmd/attachments"
 	"github.com/Korrnals/gotr/cmd/get"
+	"github.com/Korrnals/gotr/cmd/labels"
 	"github.com/Korrnals/gotr/cmd/result"
 	"github.com/Korrnals/gotr/cmd/run"
 	"github.com/Korrnals/gotr/cmd/sync"
@@ -34,6 +35,7 @@ func init() {
 	// Регистрация команд из подпакетов (передаем GetClient)
 	attachments.Register(rootCmd, GetClient)
 	get.Register(rootCmd, GetClient)
+	labels.Register(rootCmd, GetClient)
 	run.Register(rootCmd, GetClient)
 	result.Register(rootCmd, GetClient)
 	sync.Register(rootCmd, GetClient)

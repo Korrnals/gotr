884e2bed6c8ad27df307cca28922cadc7425f30a
Leonid Golikhin <korrnals@gmail.com>
2026-03-26 17:17:19 +0300

    feat(roles): add interactive role lookup

--- cmd/roles/get.go
+++ cmd/roles/get.go
@@ -12,7 +12,7 @@ import (
 // Эндпоинт: GET /get_role/{role_id}
 func newGetCmd(getClient GetClientFunc) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "get <role_id>",
+		Use:   "get [role_id]",
 		Short: "Получить роль по ID",
 		Long: `Получает информацию о роли пользователя по её ID.
 
@@ -23,11 +23,23 @@ func newGetCmd(getClient GetClientFunc) *cobra.Command {
 
   # Сохранить в файл
   gotr roles get 3 -o role.json`,
-		Args: cobra.ExactArgs(1),
+		Args: cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			roleID, err := flags.ValidateRequiredID(args, 0, "role_id")
-			if err != nil {
-				return err
+			var roleID int64
+			var err error
+			if len(args) > 0 {
+				roleID, err = flags.ValidateRequiredID(args, 0, "role_id")
+				if err != nil {
+					return err
+				}
+			} else {
+				if err := requireInteractiveRoleArg(cmd.Context(), "gotr roles get [role_id]"); err != nil {
+					return err
+				}
+				roleID, err = resolveRoleIDInteractive(cmd.Context(), getClient(cmd))
+				if err != nil {
+					return err
+				}
 			}
 
 			cli := getClient(cmd)
